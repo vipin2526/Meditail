973b9db64c568d292fff6846446ae2fd2715a925
vipin kumar <xmartcoder@gmail.com>
2023-10-25 09:51:55 +0530

    starting

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+ 
+function App() {
+  return (
+    <div className="App text-center">
+      Welcome to Meditail
+    </div>
+  ); 
+}
+
+export default App;
