5138247b723ba33896ae8416c944cdff0fe00a16
Shivendra Ojha <96243341+Shivendraojha@users.noreply.github.com>
2023-10-25 13:12:28 +0000

    redux install check

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Counter } from './features/counter/Counter';
 import "./App.css"
 function App() {
   return (
     <div className="App">
       Welcome to Meditail
+      <Counter />
     </div>
   ); 
 }
