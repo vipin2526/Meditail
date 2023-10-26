ac92c787d128010831ae24f851928f050b74405c
Shivendra Ojha <96243341+Shivendraojha@users.noreply.github.com>
2023-10-26 19:26:26 +0000

    s

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <div className='header'>
       <ul className='headerRoutes'>
-        <li style={{ marginRight: 'auto' }}> <img src="/Logo192.png" alt="not found" height={40} /> </li>
+        <li style={{ marginRight: 'auto' }}> <img src="/logo192.png" alt="not found" height={40} /> </li>
         <li><Link to={'/'}>HOME</Link></li>
         <li><Link to={'/treatments'}>TREATMENTS</Link></li>
         <li><Link to={'/hospital'}>HOSPITAL</Link></li>
