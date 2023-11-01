9107608664625bc3cf409257f2475a57982ba369
Shivendra Ojha <96243341+Shivendraojha@users.noreply.github.com>
2023-11-01 18:39:23 +0000

    profile page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,7 +3,7 @@ import '../App.css'
 export default function LandingPage() {
   return (
     <div>
-      <div className='slogan'>WE CARE OF YOUR HEALTH</div>
+      <div className='slogan'>"WE CARE OF YOUR HEALTH"</div>
       <div className='sloganpara'>"We prioritize your well-being, offering expert guidance and support. Your health is our top concern, and we are committed to providing exceptional care and understanding."</div>
       <div className='notice'>
         <div className='services'>WE CARE OF YOUR HEALTH</div>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,11 +6,53 @@ import icon from '../asset/icon.jpg'
 export default function Profile() {
     return (
         <div className="container-fluid profileBox">
-            <div className="profileIcon">
-                <img src={icon} alt="Not Found" height={100} />
+
+            <div className="userProfile">
+                <div className="profileIcon">
+                    <img src={icon} alt="Not Found" height={100} />
+                    <h1>Patient Name</h1>
+                    <h1>DOB : 20/20/2020</h1>
+                    <h1>Weight : 20kg</h1>
+                    <h1>Gender : male</h1>
+                    <h1>Blood group : B+</h1>
+                    <h1>Moile : XXXXXXXXXX</h1>
+                    <h1>Adhar :</h1>
+                    <h1>XXXX XXXX XXXX</h1>
+                </div>
             </div>
-            <div className="userName">Patient</div>
-            <h3>some other detials</h3>
+
+            <div className="userDetail">
+
+                <h1 className='History heading'>HISTORY</h1>
+                <l1 className="Medical-History list">
+                    <h1 className='medicineDate'>20/20/2020 : Doctor</h1> 
+                    <h3 className='medicineName'>Medicine :</h3>
+                    <h3 className='medicineName'>Medicine :</h3>
+                    <h3 className='medicineName'>Medicine :</h3>
+                </l1>
+                <l1 className="Medical-History list">
+                    <h1 className='medicineDate'>20/20/2020 : Doctor</h1> 
+                    <h3 className='medicineName'>Medicine :</h3>
+                    <h3 className='medicineName'>Medicine :</h3>
+                    <h3 className='medicineName'>Medicine :</h3>
+                </l1>
+                <l1 className="Medical-History list">
+                    <h1 className='medicineDate'>20/20/2020 : Doctor</h1> 
+                    <h3 className='medicineName'>Medicine :</h3>
+                    <h3 className='medicineName'>Medicine :</h3>
+                    <h3 className='medicineName'>Medicine :</h3>
+                </l1>
+                <l1 className="Medical-History list">
+                    <h1 className='medicineDate'>20/20/2020 : Doctor</h1> 
+                    <h3 className='medicineName'>Medicine :</h3>
+                    <h3 className='medicineName'>Medicine :</h3>
+                    <h3 className='medicineName'>Medicine :</h3>
+                </l1>
+
+
+
+            </div>
+
 
         </div>
     )
