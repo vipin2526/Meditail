c9ea04d15b27bd6f64758598f0519b773d221de7
vipin2526 <vipinkushwaha2526@gmail.com>
2024-07-11 20:01:06 +0530

    just

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import icon from '../asset/icon.jpg'
-import '../components/css/profile.css'
+import '../components/css/Profile.css'
 
 export default function Profile() {
     return (
