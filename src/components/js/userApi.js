da6e57c8bad132f2125ba1da979700571615ce42
Vipin Kushwaha <VipinKushwaha2526@gmail.com>
2024-08-05 18:36:25 +0000

    just

--- src/components/js/userApi.js
+++ src/components/js/userApi.js
@@ -1,8 +1,8 @@
 // api.js
 import axios from 'axios';
 
-const backendUrl = 'http://localhost:3001';
-// const backendUrl = 'https://meditail-backend.vercel.app';
+// const backendUrl = 'http://localhost:3001';
+const backendUrl = 'https://meditail-backend.vercel.app';
 
 const userApi = axios.create({
     baseURL: backendUrl,
